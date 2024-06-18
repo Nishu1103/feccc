45fd72ce2b851ac3d1dee3ae389b7a53933fb561
nishant <nishantk1103@gmail.com>
2024-06-18 22:30:38 +0530

    touch contact

--- src/Component/Team/Team.jsx
+++ src/Component/Team/Team.jsx
@@ -11,68 +11,68 @@ export default function Team() {
           {
             name: 'Divyansh Gupta',
             image: './contact/Divyansh.webp',
-            linkedin: 'https://www.linkedin.com/in/alice-smith',
-            facebook: 'https://www.facebook.com/alice.smith'
+            linkedin: 'https://www.linkedin.com/in/divyanshhgupta/',
+            facebook: 'https://www.facebook.com/profile.php?id=100087196202941'
           },
           {
             name: 'Ketul Sagar',
             image: './contact/Ketul_Sagar.webp',
-            linkedin: 'https://www.linkedin.com/in/david-johnson',
-            facebook: 'https://www.facebook.com/david.johnson'
+            linkedin: 'https://www.linkedin.com/in/ketul-sagar?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
+            facebook: 'https://www.facebook.com/profile.php?id=100088404151277'
           },
           {
             name: 'Aman gupta ',
             image: './contact/AmanGupta.webp',
-            linkedin: 'https://www.linkedin.com/in/david-johnson',
-            facebook: 'https://www.facebook.com/david.johnson'
+            linkedin: 'https://www.linkedin.com/in/aman-gupta-134ab4230/',
+            facebook: 'https://www.facebook.com/'
           },
           {
             name: 'Jagrut Karnavat ',
             image: './contact/Divysh.webp',
-            linkedin: 'https://www.linkedin.com/in/david-johnson',
-            facebook: 'https://www.facebook.com/david.johnson'
+            linkedin: 'https://www.linkedin.com/in/jagrut-karnavat-7b0b28257?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            facebook: 'https://www.facebook.com/JKThunder279?mibextid=ZbWKwL'
           },
           {
             name: 'Aviral Singh',
             image: './contact/aviralsingh.webp',
-            linkedin: 'https://www.linkedin.com/in/david-johnson',
-            facebook: 'https://www.facebook.com/david.johnson'
+            linkedin: 'https://www.linkedin.com/in/aviral-singh-6180a1285?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            facebook: 'https://www.facebook.com/profile.php?id=100087466934423'
           },
           {
             name: 'Riya Salunkhe',
             image: './contact/RiyaSalunkhe.webp',
-            linkedin: 'https://www.linkedin.com/in/david-johnson',
-            facebook: 'https://www.facebook.com/david.johnson'
+            linkedin: 'https://www.linkedin.com/in/riyasalunkhe1853?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
+            facebook: 'https://www.facebook.com/'
           },
           {
             name: 'Shriyansh asati',
             image: './contact/Diyansh.webp',
-            linkedin: 'https://www.linkedin.com/in/david-johnson',
-            facebook: 'https://www.facebook.com/david.johnson'
+            linkedin: 'https://www.linkedin.com/',
+            facebook: 'https://www.facebook.com'
           },
           {
             name: 'Pranav Senthilkumar',
             image: './contact/PranavSenthilkumar.webp',
-            linkedin: 'https://www.linkedin.com/in/david-johnson',
-            facebook: 'https://www.facebook.com/david.johnson'
+            linkedin: 'https://www.linkedin.com',
+            facebook: 'https://www.facebook.com/profile.php?id=100041830773249&mibextid=ZbWKwL'
           },
           {
             name: 'Shivee Gupta',
             image: './contact/ShiveeGupta.webp',
-            linkedin: 'https://www.linkedin.com/in/david-johnson',
-            facebook: 'https://www.facebook.com/david.johnson'
+            linkedin: 'https://www.linkedin.com/in/shivee-gupta-544429255/',
+            facebook: 'https://www.facebook.com/profile.php?id=100086993780456'
           },
           {
             name: 'Ritabrata Bharati',
             image: './contact/Ritabrata.webp',
-            linkedin: 'https://www.linkedin.com/in/david-johnson',
-            facebook: 'https://www.facebook.com/david.johnson'
+            linkedin: 'https://www.linkedin.com/in/ritabrata-bharati-a724a824a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            facebook: 'https://www.facebook.com/profile.php?id=100087764766972&mibextid=ZbWKwL'
           },
           {
             name: 'Saurabh Raj',
             image: './contact/SaurabhRaj.webp',
-            linkedin: 'https://www.linkedin.com/in/david-johnson',
-            facebook: 'https://www.facebook.com/david.johnson'
+            linkedin: 'https://www.linkedin.com/in/saurabh-raj-669999258?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            facebook: 'https://www.facebook.com/share/AX1RA3eDkPi115bS/?mibextid=qi2Omg'
           }
         ]
       },
@@ -82,26 +82,26 @@ export default function Team() {
           {
             name: 'Jatain Kumar ',
             image: './contact/Divyans.webp',
-            linkedin: 'https://www.linkedin.com/in/bob-johnson',
-            facebook: 'https://www.facebook.com/bob.johnson'
+            linkedin: 'https://www.linkedin.com/in/jatainkumar?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            facebook: 'https://www.facebook.com/jatain.kumar.17?mibextid=ZbWKwL'
           },
           {
             name: 'Ashwini Raj',
             image: './contact/Ashwini_Raj.webp',
-            linkedin: 'https://www.linkedin.com/in/emily-clark',
-            facebook: 'https://www.facebook.com/emily.clark'
+            linkedin: 'https://www.linkedin.com',
+            facebook: 'https://www.facebook.com'
           },
           {
             name: 'Palak Agrawal ',
             image: './contact/PalakAgrawal.webp',
-            linkedin: 'https://www.linkedin.com/in/david-johnson',
-            facebook: 'https://www.facebook.com/david.johnson'
+            linkedin: 'https://www.linkedin.com/in/palak-agrawal-0a546724a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            facebook: 'https://www.facebook.com/profile.php?id=100086204021248&mibextid=ZbWKwL'
           },
           {
             name: 'Aishwarya Maan Srivastava',
             image: './contact/AishwaryaMaanSrivastava.webp',
-            linkedin: 'https://www.linkedin.com/in/david-johnson',
-            facebook: 'https://www.facebook.com/david.johnson'
+            linkedin: 'https://www.linkedin.com/in/aishwarya-maan-srivastava-b883a1255/',
+            facebook: 'https://www.facebook.com/profile.php?id=100087460456348'
           }
         ]
       },

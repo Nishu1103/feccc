941dddcb850b6daf8750fa4bdd7e6230855ca2e7
nishant <nishantk1103@gmail.com>
2024-07-02 09:25:17 +0530

    mdinind

--- src/Component/Home/Landingpage.jsx
+++ src/Component/Home/Landingpage.jsx
@@ -4,7 +4,7 @@ export default function (){
     return (
         <>
         <div className="landing-page">
-            <img src="images/4.png" alt="Landing Page Image" />
+            <img src="images/teams.png" alt="Landing Page Image" />
             <div className="arrows">
                 <span className="arrow"></span>
                 <span className="arrow"></span>

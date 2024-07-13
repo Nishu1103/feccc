234766416240d3187705f5a0588581d79e0180a6
ReshuGupta2004 <riyanshigupta2004@gmail.com>
2024-07-14 00:12:13 +0530

    hhhhhh

--- src/Component/MyPreviousWork/MyPreviousWork.jsx
+++ src/Component/MyPreviousWork/MyPreviousWork.jsx
@@ -3,28 +3,28 @@ import './MyPreviousWork.css';
 
 const works = [
   {
-    title: 'Project 1',
-    image: 'images/teams.png',
+    title: 'Workshop on Quantitative Finance',
+    image: 'images/post1.jpeg',
     description: 'Description for project 1',
-    link: '#'
+    link: 'https://www.linkedin.com/posts/fec-iit-kharagpur_iitkgp-quant-finance-activity-7209834129436168192-hHbw?utm_source=share&utm_medium=member_desktop'
   },
   {
-    title: 'Project 2',
-    image: 'images/teams.png',
+    title: 'Our Quantitative Finance Projects',
+    image: 'images/post2.jpeg',
     description: 'Description for project 2',
-    link: '#'
+    link: 'https://www.linkedin.com/posts/fec-iit-kharagpur_finance-dataanalysis-financeprojects-activity-7211975164329095168-hiy8?utm_source=share&utm_medium=member_desktop'
   },
   {
-    title: 'Project 3',
-    image: 'images/teams.png',
+    title: 'Roadmap on Finance',
+    image: 'images/ddd.png',
     description: 'Description for project 3',
-    link: '#'
+    link: 'https://medium.com/@fec.iitkgp/exploring-career-paths-in-finance-acaf3f438ca1'
   },
   {
-    title: 'Project 4',
-    image: 'images/teams.png',
+    title: 'Summer of Finance',
+    image: 'images/dd.png',
     description: 'Description for project 4',
-    link: '#'
+    link: 'https://boatneck-cougar-e3c.notion.site/SUMMER-OF-FINANCE-79cc8e8d272b479e9a8fcf1e916314ad'
   }
 ];
 

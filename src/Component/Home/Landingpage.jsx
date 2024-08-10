244aabd46969595d57ece993a12badd7be738cd1
Cj9520 <jindalchirag7417@kgpian.iitkgp.ac.in>
2024-08-11 00:54:48 +0530

    homepage

--- src/Component/Home/Landingpage.jsx
+++ src/Component/Home/Landingpage.jsx
@@ -1,49 +1,109 @@
-import { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./landing.css";
+import Animatedletters from "./Animetedlatter";
+import Typewriter from "./typewriter";
 
-export default function LandingPage() {
+const LandingPage = () => {
   const vantaRef = useRef(null);
+  const [letterClass, setLetters] = useState('text-animate')
+  const FArray = ['F', 'i', 'n', 'a', 'n', 'c', 'e', ' ']
+  const AArray = ['a', 'n', 'd']
+  const EArray = [' ', 'E', 'c', 'o', 'n', 'o', 'm', 'i', 'c', 's', ' ', 'C', 'l', 'u', 'b']
+
+  const placeArray = ['I', 'I', 'T', ' ', 'K', 'H', 'A', 'R', 'A', 'G', 'P', 'U', 'R']
 
   useEffect(() => {
-    // Wait until the global VANTA is available
-    const initVanta = () => {
-      if (window.VANTA) {
-        const VANTA = window.VANTA;
-        const THREE = window.THREE;
-
-        const effect = VANTA.NET({
-          el: vantaRef.current,
-          mouseControls: true,
-          touchControls: true,
-          gyroControls: false,
-          minHeight: 200.0,
-          minWidth: 200.0,
-          scale: 1.0,
-          scaleMobile: 0.5,
-          color: 0x88dbda,
-          backgroundColor: 0xffffff,
-        //   showDots: false,
-          points: 7.00,
-          maxDistance: 15.00,
-          spacing: 15.00
-        });
-
-        return effect;
-      }
-    };
-
-    const effect = initVanta();
+    const timeoutId = setTimeout(() => {
+      setLetters('text-animate-hover')
+    }, 4000)
 
     return () => {
-      if (effect && typeof effect.destroy === "function") {
-        effect.destroy();
-      }
-    };
-  }, []);
+      clearTimeout(timeoutId)
+    }
+  }, [])
+
+  return (
+    <section >
+      <div id="main">
+        <section id="left">
+          <div className="heading">
+            <h1 style={{ fontSize: '35px', fontWeight: '700' }}>
+              <span>
+                <Animatedletters
+                  letterClass={letterClass}
+                  strArray={FArray}
+                  idx={15}
+                />
+              </span>
+              <span style={{ fontWeight: '400' }}><Animatedletters
+                letterClass={letterClass}
+                strArray={AArray}
+                idx={23}
+              /></span>
+              <span><Animatedletters
+                letterClass={letterClass}
+                strArray={EArray}
+                idx={26}
+              /></span>
+              <br />
+              <span style={{ color: '#63BB54' }}>
+                <Animatedletters
+                  letterClass={letterClass}
+                  strArray={placeArray}
+                  idx={42}
+                />
+              </span>
+            </h1>
+          </div>
+          <br />
+          <div>
+            <Typewriter textArray={['Be an asset to everyone...', 'Not a liability !!!']} period={1000} />
+            {/* // className="typewriter link-light"
+            // data-period="4000"
+            // data-type='[ "Be an asset to everyone", "Not a liability !!!"]'
+            // style={{ fontSize: '25px', fontWeight: 800, fontFamily: 'Manrope' }} */}
+          </div>
+          <div className="note">
+            <p style={{ fontSize: '18px', textAlign: 'justify', fontWeight: 800 }}>
+              The Finance and Economics Club (FEC) functions as a platform for enthusiastic students to come together and learn the intriguing and fun world of finance, by organizing introductory sessions for people who are interested in finance but do not have any prior knowledge, through inviting Industry Leaders and alumni
+              <br /><br />
+            </p>
+          </div>
+
+          <br />
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              marginLeft: '0%',
+              padding: '2% 5%',
+              color: '#63BB54',
+              border: '#f1dc8d',
+              borderRadius: '10px',
+              borderStyle: 'solid'
+            }}
+          >
+            Register for Future Events Here
+          </a>
+        </section>
+        <section id="right" >
+          <img src="images/logo.png" alt="Main Right" style={{ maxWidth: '100%' }} />
+        </section>
+      </div>
+    </section>
+  );
+
+
+  const effect = initVanta();
+
 
   return (
     <div className="landing-page" ref={vantaRef}>
-      <img src="images/text.png" alt="Landing Page Image" />
+      {/* <img src="images/text.png" alt="Landing Page Image" /> */}
     </div>
+
   );
 }
+
+export default LandingPage;
